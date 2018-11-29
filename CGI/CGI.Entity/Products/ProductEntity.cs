7b5214519fe54789a2b28f99577d0c09d4cfa5c4
Ricardo Nunes <ricardo.jf.nunes@outlook.pt>
2018-11-29 21:44:34 +0000

    Added the Entitys ClassLibrary. Added the Product and User Entitys

---
+++ CGI/CGI.Entity/Products/ProductEntity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace CGI.Entity.Products
+{
+    [Table("Product", Schema = "dbo")]
+    public class ProductEntity
+    {
+        [Key]
+        [Column("Id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Column("SerialNumber")]
+        public string SerialNumber { get; set; }
+
+        [Column("Brand")]
+        public string Brand { get; set; }
+
+        [Column("Model")]
+        public string Model { get; set; }
+
+        [Column("ProductType")]
+        public string ProductType { get; set; }
+    }
+}
---
+++ CGI/CGI.Entity/User/UserEntity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace CGI.Entity.User
+{
+    [Table("UserSignIn", Schema = "dbo")]
+    public class UserEntity
+    {
+        [Key]
+        [Column("Id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Column("Name")]
+        public string Name { get; set; }
+
+        [Column("Email")]
+        public string Email { get; set; }
+
+        [Column("Password")]
+        public string Password { get; set; }
+
+        [Column("LoginByPin")]
+        public bool LoginByPin { get; set; }
+         
+        [Column("PinCode")]
+        public int? PinCode { get; set; }
+    }
+}
